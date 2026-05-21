7eaaab8e595609af303a4cb172b5d37188a6d332
nata13-dot <natanaelbarrera@hotmail.com>
2026-05-20 21:37:13 -0600

    Actualizar bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->api(prepend: [
-            \Illuminate\Http\Middleware\HandleCors::class,
             \App\Http\Middleware\SanitizeApiInput::class,
         ]);
         
